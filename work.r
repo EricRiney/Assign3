37d4c34725b29a41272bbeda8e5d9db417701b45
EricRiney <fails.to.install@gmail.com>
2016-11-22 20:36:53 -0800

    blah

--- work.r
+++ work.r
@@ -13,7 +13,92 @@ summary(crime)
 
 #GOAL: FIND OUT IF THERE IS A CORRELATION BETWEEN WEATHER AND CRIME RATES
 
-hist(weather$Temp.Avg)
+hist(weather$AvgTemp....F.)
+
+# Monthly Dataframes for 2011
+AUG2011df = crime[1:sum(crime$Month == "8" & crime$Year == "2011"),]
+SEP2011df = crime[1:sum(crime$Month == "9" & crime$Year == "2011"),]
+OCT2011df = crime[1:sum(crime$Month == "10" & crime$Year == "2011"),]
+NOV2011df = crime[1:sum(crime$Month == "11" & crime$Year == "2011"),]
+DEC2011df = crime[1:sum(crime$Month == "12" & crime$Year == "2011"),]
+
+# Monthly Dataframes for 2012
+JAN2012df = crime[1:sum(crime$Month == "1" & crime$Year == "2012"),]
+FEB2012df = crime[1:sum(crime$Month == "2" & crime$Year == "2012"),]
+MAR2012df = crime[1:sum(crime$Month == "3" & crime$Year == "2012"),]
+APR2012df = crime[1:sum(crime$Month == "4" & crime$Year == "2012"),]
+MAY2012df = crime[1:sum(crime$Month == "5" & crime$Year == "2012"),]
+JUN2012df = crime[1:sum(crime$Month == "6" & crime$Year == "2012"),]
+JLY2012df = crime[1:sum(crime$Month == "7" & crime$Year == "2012"),]
+AUG2012df = crime[1:sum(crime$Month == "8" & crime$Year == "2012"),]
+SEP2012df = crime[1:sum(crime$Month == "9" & crime$Year == "2012"),]
+OCT2012df = crime[1:sum(crime$Month == "10" & crime$Year == "2012"),]
+NOV2012df = crime[1:sum(crime$Month == "11" & crime$Year == "2012"),]
+DEC2012df = crime[1:sum(crime$Month == "12" & crime$Year == "2012"),]
+
+# Monthly Dataframes for 2013
+JAN2013df = crime[1:sum(crime$Month == "1" & crime$Year == "2013"),]
+FEB2013df = crime[1:sum(crime$Month == "2" & crime$Year == "2013"),]
+MAR2013df = crime[1:sum(crime$Month == "3" & crime$Year == "2013"),]
+APR2013df = crime[1:sum(crime$Month == "4" & crime$Year == "2013"),]
+MAY2013df = crime[1:sum(crime$Month == "5" & crime$Year == "2013"),]
+JUN2013df = crime[1:sum(crime$Month == "6" & crime$Year == "2013"),]
+JLY2013df = crime[1:sum(crime$Month == "7" & crime$Year == "2013"),]
+AUG2013df = crime[1:sum(crime$Month == "8" & crime$Year == "2013"),]
+SEP2013df = crime[1:sum(crime$Month == "9" & crime$Year == "2013"),]
+OCT2013df = crime[1:sum(crime$Month == "10" & crime$Year == "2013"),]
+NOV2013df = crime[1:sum(crime$Month == "11" & crime$Year == "2013"),]
+DEC2013df = crime[1:sum(crime$Month == "12" & crime$Year == "2013"),]
+
+# Monthly Dataframes for 2014
+JAN2014df = crime[1:sum(crime$Month == "1" & crime$Year == "2014"),]
+FEB2014df = crime[1:sum(crime$Month == "2" & crime$Year == "2014"),]
+MAR2014df = crime[1:sum(crime$Month == "3" & crime$Year == "2014"),]
+APR2014df = crime[1:sum(crime$Month == "4" & crime$Year == "2014"),]
+MAY2014df = crime[1:sum(crime$Month == "5" & crime$Year == "2014"),]
+JUN2014df = crime[1:sum(crime$Month == "6" & crime$Year == "2014"),]
+JLY2014df = crime[1:sum(crime$Month == "7" & crime$Year == "2014"),]
+AUG2014df = crime[1:sum(crime$Month == "8" & crime$Year == "2014"),]
+SEP2014df = crime[1:sum(crime$Month == "9" & crime$Year == "2014"),]
+OCT2014df = crime[1:sum(crime$Month == "10" & crime$Year == "2014"),]
+NOV2014df = crime[1:sum(crime$Month == "11" & crime$Year == "2014"),]
+DEC2014df = crime[1:sum(crime$Month == "12" & crime$Year == "2014"),]
+
+# Monthly Dataframes for 2015
+JAN2015df = crime[1:sum(crime$Month == "1" & crime$Year == "2015"),]
+FEB2015df = crime[1:sum(crime$Month == "2" & crime$Year == "2015"),]
+MAR2015df = crime[1:sum(crime$Month == "3" & crime$Year == "2015"),]
+APR2015df = crime[1:sum(crime$Month == "4" & crime$Year == "2015"),]
+MAY2015df = crime[1:sum(crime$Month == "5" & crime$Year == "2015"),]
+JUN2015df = crime[1:sum(crime$Month == "6" & crime$Year == "2015"),]
+JLY2015df = crime[1:sum(crime$Month == "7" & crime$Year == "2015"),]
+AUG2015df = crime[1:sum(crime$Month == "8" & crime$Year == "2015"),]
+SEP2015df = crime[1:sum(crime$Month == "9" & crime$Year == "2015"),]
+OCT2015df = crime[1:sum(crime$Month == "10" & crime$Year == "2015"),]
+NOV2015df = crime[1:sum(crime$Month == "11" & crime$Year == "2015"),]
+DEC2015df = crime[1:sum(crime$Month == "12" & crime$Year == "2015"),]
+
+# Monthly Dataframes for 2016
+JAN2016df = crime[1:sum(crime$Month == "1" & crime$Year == "2016"),]
+FEB2016df = crime[1:sum(crime$Month == "2" & crime$Year == "2016"),]
+MAR2016df = crime[1:sum(crime$Month == "3" & crime$Year == "2016"),]
+APR2016df = crime[1:sum(crime$Month == "4" & crime$Year == "2016"),]
+MAY2016df = crime[1:sum(crime$Month == "5" & crime$Year == "2016"),]
+JUN2016df = crime[1:sum(crime$Month == "6" & crime$Year == "2016"),]
+JLY2016df = crime[1:sum(crime$Month == "7" & crime$Year == "2016"),]
+AUG2016df = crime[1:sum(crime$Month == "8" & crime$Year == "2016"),]
+SEP2016df = crime[1:sum(crime$Month == "9" & crime$Year == "2016"),]
+OCT2016df = crime[1:sum(crime$Month == "10" & crime$Year == "2016"),]
+NOV2016df = crime[1:sum(crime$Month == "11" & crime$Year == "2016"),]
+DEC2016df = crime[1:sum(crime$Month == "12" & crime$Year == "2016"),]
+
+t.test(AUG2011df, SEP2011df)
+
+totalCrimes = sum(complete.cases(crime$Month))
+
+
+
+### Ash Code ###
 (data.frame(table(crime$Event.Clearance.Date)))
 table(format(crime$Event.Clearance.Date, "%b-%Y"))
 
