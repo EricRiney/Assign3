9bb52c01cbdf4d483169542504995b8171a43c92
EricRiney <fails.to.install@gmail.com>
2016-12-01 15:37:21 -0800

    balh

--- bigSets.R
+++ bigSets.R
@@ -6,38 +6,21 @@ setwd("/Users/ericriner/Documents/Code/UW/370/Assign3")
 weather = read.csv(file.choose(), header = TRUE)
 crime = read.csv(file.choose(), header = TRUE)
 
-weatherAll = weather[(weather$Year == '2012' || 
-                        weather$Year == '2013' || 
-                        weather$Year == '2014' || 
-                        weather$Year == '2015'),]
-weatherAllResults = vector(mode="numeric", length=nrow(weatherAll))
+weatherAll = weather[(weather$Year == '2015'),]
+weatherAllResults = weatherAll[(weatherAll$Precip...in. != 0),]
 
-for (i in 1:nrow(weatherAll)) {
-  #counter = 0
-  #for (j in 1:length(weatherAllResults)) {
-    if(weatherAll$Precip...in.[i] != 0) {
-      weatherAllResults[i] = 1
-    }
-    # weatherAllResults[i] = (counter)
-  }
-#}
+crimeAllBURGLARY = crime[(crime$Summarized.Offense.Description == 'BURGLARY' &  crime$Year == '2015'),]
 
-weatherClum2012DatesResults[weatherClum2012DatesResults == 'T'] <- 0.2
-
-crimeAllBURGLARY = crime[(#crime$Summarized.Offense.Description == 'BURGLARY' || 
-                            crime$Year == '2012' || 
-                            crime$Year == '2013' || 
-                            crime$Year == '2014' || 
-                            crime$Year == '2015'),]
+bork = match(weatherAll$Month,month.abb)
+meow = crimeAllBURGLARY$Month
 
 crimeAllBURGLARYResults = vector(mode="numeric", length=nrow(weatherAll))
-for (i in 1:nrow(crimeAllBURGLARY)) {
-  counter = 0
-  if(crimeAllBURGLARY$Summarized.Offense.Description[i] == 'BURGLARY'){
-    counter = counter + 1
+
+for (i in 1:nrow(crimeAllBURGLARY )) {
+    if(crimeAllBURGLARY$Month[i] != 0) {
+      crimeAllBURGLARYResults[i] = 1
+    }
   }
-  crimeAllBURGLARYResults[i] = counter
-}
 
 cor(crimeAllBURGLARYResults,weatherAllResults)
 
