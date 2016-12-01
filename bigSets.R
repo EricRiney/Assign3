e6c44206fea2cc32e4e52bcf41424129dc2ae7e1
EricRiney <fails.to.install@gmail.com>
2016-11-30 22:26:04 -0800

    bigSets added

---
+++ bigSets.R
@@ -0,0 +1,7 @@
+# Eric 
+# INFO 370
+
+setwd("/Users/ericriner/Documents/Code/UW/370/Assign3")
+
+weather = read.csv(file.choose(), header = TRUE)
+crime = read.csv(file.choose(), header = TRUE)
--- crimeNorm.R
+++ crimeNorm.R
@@ -116,3 +116,72 @@ for (i in 1:length(weaponsCrime2012Vec)) {
 }
 summary(weaponsCrime2012Results)
 cor(weather2012DatesResults,weaponsCrime2012Results)
+
+### 2012 bikeTheft Crime info ###
+bikeTheftCrime2012Vec = (seq(as.Date("2012-1-01"), as.Date("2012-12-31"), by="+1 day"))
+bikeTheftCrime2012Results = vector(mode="numeric", length=length(bikeTheftCrime2012Vec))
+for (i in 1:length(bikeTheftCrime2012Vec)) {
+  counter = 0
+  if(crime$Summarized.Offense.Description[i] == 'BIKE THEFT'){
+    counter = counter + 1
+  }
+  bikeTheftCrime2012Results[i] = counter
+}
+summary(bikeTheftCrime2012Results)
+cor(weather2012DatesResults,bikeTheftCrime2012Results)
+
+
+### 2012 narcotics Crime info ###
+narcoticsCrime2012Vec = (seq(as.Date("2012-1-01"), as.Date("2012-12-31"), by="+1 day"))
+narcoticsCrime2012Results = vector(mode="numeric", length=length(narcoticsCrime2012Vec))
+for (i in 1:length(narcoticsCrime2012Vec)) {
+  counter = 0
+  if(crime$Summarized.Offense.Description[i] == 'NARCOTICS'){
+    counter = counter + 1
+  }
+  narcoticsCrime2012Results[i] = counter
+}
+summary(narcoticsCrime2012Results)
+cor(weather2012DatesResults,narcoticsCrime2012Results)
+
+
+### 2012 fraud Crime info ###
+fraudCrime2012Vec = (seq(as.Date("2012-1-01"), as.Date("2012-12-31"), by="+1 day"))
+fraudCrime2012Results = vector(mode="numeric", length=length(fraudCrime2012Vec))
+for (i in 1:length(fraudCrime2012Vec)) {
+  counter = 0
+  if(crime$Summarized.Offense.Description[i] == 'FRAUD'){
+    counter = counter + 1
+  }
+  fraudCrime2012Results[i] = counter
+}
+summary(fraudCrime2012Results)
+cor(weather2012DatesResults,fraudCrime2012Results)
+
+
+### 2012 liquorViolation info ###
+liquorViolationCrime2012Vec = (seq(as.Date("2012-1-01"), as.Date("2012-12-31"), by="+1 day"))
+liquorViolationCrime2012Results = vector(mode="numeric", length=length(liquorViolationCrime2012Vec))
+for (i in 1:length(liquorViolationCrime2012Vec)) {
+  counter = 0
+  if(crime$Summarized.Offense.Description[i] == 'LIQUOR VIOLATION'){
+    counter = counter + 1
+  }
+  liquorViolationCrime2012Results[i] = counter
+}
+summary(liquorViolationCrime2012Results)
+cor(weather2012DatesResults,liquorViolationCrime2012Results)
+
+### 2012 animalComplaint info ###
+animalComplaintCrime2012Vec = (seq(as.Date("2012-1-01"), as.Date("2012-12-31"), by="+1 day"))
+animalComplaintCrime2012Results = vector(mode="numeric", length=length(animalComplaintCrime2012Vec))
+for (i in 1:length(animalComplaintViolationCrime2012Vec)) {
+  counter = 0
+  if(crime$Summarized.Offense.Description[i] == 'ANIMAL COMPLAINT'){
+    counter = counter + 1
+  }
+  animalComplaintCrime2012Results[i] = counter
+}
+summary(animalComplaintCrime2012Results)
+cor(weather2012DatesResults,animalComplaintCrime2012Results)
+
