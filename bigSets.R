c5d62a6b5abe0a4e0a161182afcddf9c03024e6b
EricRiney <fails.to.install@gmail.com>
2016-12-01 15:51:14 -0800

    blah

--- bigSets.R
+++ bigSets.R
@@ -10,17 +10,8 @@ weatherAll = weather[(weather$Year == '2015'),]
 weatherAllResults = weatherAll[(weatherAll$Precip...in. != 0),]
 
 crimeAllBURGLARY = crime[(crime$Summarized.Offense.Description == 'BURGLARY' &  crime$Year == '2015'),]
+year = vector(mode="numeric", length=365)
 
-bork = match(weatherAll$Month,month.abb)
-meow = crimeAllBURGLARY$Month
-
-crimeAllBURGLARYResults = vector(mode="numeric", length=nrow(weatherAll))
-
-for (i in 1:nrow(crimeAllBURGLARY )) {
-    if(crimeAllBURGLARY$Month[i] != 0) {
-      crimeAllBURGLARYResults[i] = 1
-    }
-  }
 
 cor(crimeAllBURGLARYResults,weatherAllResults)
 
