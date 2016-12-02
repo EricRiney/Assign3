59cb78cf12a3e5dab6dbb98c2c3fadd4594d7f34
EricRiney <fails.to.install@gmail.com>
2016-12-01 21:41:24 -0800

    blah

--- bigSets.R
+++ bigSets.R
@@ -7,9 +7,14 @@ weather = read.csv(file.choose(), header = TRUE)
 crime = read.csv(file.choose(), header = TRUE)
 
 weatherAll = weather[(weather$Year == '2015'),]
-weatherAllResults = weatherAll[(weatherAll$Precip...in. != 0),]
+grainWeatherAllResults = data.frame((paste(match(weatherAll$Month,month.abb), weatherAllResults$Date,sep="-")))
 
+
+weatherAll$Date = grainWeatherAllResults
 crimeAllBURGLARY = crime[(crime$Summarized.Offense.Description == 'BURGLARY' &  crime$Year == '2015'),]
+grainCrimeAllBURGLARY = format(as.POSIXct(strptime(crimeAllBURGLARY$Date.Reported,
+                                                   "%m/%d/%Y %H:%M",tz="")) ,format = "%m-%d")
+
 year = vector(mode="numeric", length=365)
 
 
