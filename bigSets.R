509d8bc9fc6c112281daa7b080db630cd3beee41
tbauer516 <tcbauer516@gmail.com>
2016-12-02 13:19:32 -0800

    added correct dates and merge code

--- bigSets.R
+++ bigSets.R
@@ -1,22 +1,43 @@
 # Eric 
 # INFO 370
 
+require(zoo)
+library(lubridate)
+
 weather = read.csv(file.choose(), header = TRUE)
 crime = read.csv(file.choose(), header = TRUE)
 
-weatherAll = weather[(weather$Year == '2015'),]
+crimeDate = mdy_hms(crime$Event.Clearance.Date)
 
+days = day(crimeDate)
+months = month(crimeDate)
+years = year(crimeDate)
+
+# Make sure you use only numbers in the parameters, not strings (4 instead of '4')
+# If you want non continuous frames, then use rbind(frame1, frame2) to add rows together
+makeDataframe = function(startDay, endDay, startMonth, endMonth, startYear, endYear) {
+  tempFrame = crime[(days >= startDay & days <= endDay &
+                       months >= startMonth & months <= endMonth &
+                       years >= startYear & years <= endYear &
+                       !is.na(months)),]
+  return(tempFrame)
+}
+
+weatherAll = weather[(weather$Year == '2015'),]
 
 weatherAll$Month = formatC(weatherAll$Month, width = 2, format = "d", flag = "0")
 weatherAll$Date = formatC(weatherAll$Date, width = 2, format = "d", flag = "0")
 weatherAll$Date = paste(weatherAll$Month, weatherAll$Date, sep = '-')
 
-crimeAllBURGLARY = crime[(crime$Summarized.Offense.Description == 'BURGLARY' &  crime$Year == '2015'),]
-grainCrimeAllBURGLARY = data.frame(format(as.POSIXct(strptime(crimeAllBURGLARY$Date.Reported,
-                                                   "%m/%d/%Y %H:%M",tz="")) ,format = "%m-%d"))
-crimeAllBURGLARY$Date = grainCrimeAllBURGLARY
+crime$Year = year(crimeDate)
+crime$Month = formatC(month(crimeDate), width = 2, format = "d", flag = "0")
+crime$Day = formatC(day(crimeDate), width = 2, format = "d", flag = "0")
 
-mergedSet = merge(x = weatherAll, y = crimeAllBURGLARY, by.x = Date, by.y = Date, all = T)
+crimeAllBURGLARY = crime[(crime$Event.Clearance.Group == 'BURGLARY' &  crime$Year == '2015'),]
+
+crimeAllBURGLARY$Date = paste(crimeAllBURGLARY$Month, crimeAllBURGLARY$Day, sep = '-')
+
+mergedSet = merge(x = weatherAll, y = crimeAllBURGLARY, by.x = 'Date', by.y = 'Date', all = T)
 
 #year = vector(mode="numeric", length=365)
 
