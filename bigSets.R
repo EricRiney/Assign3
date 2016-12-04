02b9e4be026e2a3d8f19c564b551d6af706bb4a1
EricRiney <fails.to.install@gmail.com>
2016-12-03 22:25:49 -0800

    meow

--- bigSets.R
+++ bigSets.R
@@ -34,30 +34,6 @@ crime$Year = year(crimeDate)
 crime$Month = formatC(month(crimeDate), width = 2, format = "d", flag = "0")
 crime$Day = formatC(day(crimeDate), width = 2, format = "d", flag = "0")
 
-<<<<<<< HEAD
-crimeAllBURGLARY = crime[(crime$Event.Clearance.Group == 'BURGLARY' &  crime$Year == '2015'),]
-crimeAllBURGLARY$Date = paste(crimeAllBURGLARY$Month, crimeAllBURGLARY$Day, sep = '-')
-
-mergedSet = merge(x = weatherAll, y = crimeAllBURGLARY, by.x = 'Date', by.y = 'Date', all = T)
-
-correlationFrame = data.frame(table(mergedSet$Date)[1:(length(table(mergedSet$Date)) - 1)], weatherAll$Precip...in.)
-names(correlationFrame) = c('date', 'count', 'precip')
-
-correlationFrameAgg = aggregate(. ~  precip, data = correlationFrame, sum)
-correlationFrameAgg$precip = as.numeric(correlationFrameAgg$precip)
-correlationFrameAgg$count = as.numeric(correlationFrameAgg$count)
-
-summary(lm(log(count) ~ precip, data = correlationFrameAgg))
-cor.test(x = correlationFrameAgg$precip, y = log(correlationFrameAgg$count))
-
-dev.off()
-
-ggplot(correlationFrameAgg, aes(x = precip, y = log(count))) +
-  xlab('# of Burglaries') +
-  ylab('Amount of Precipitation in inches') +
-  geom_point(shape=1) +
-  geom_smooth(method=lm)
-=======
 getCorrelation = function(crimeName, crimeYear) {
   crimeAllBURGLARY = crime[(crime$Event.Clearance.Group == toString(crimeName) &  crime$Year == toString(crimeYear)),]
   crimeAllBURGLARY$Date = paste(crimeAllBURGLARY$Month, crimeAllBURGLARY$Day, sep = '-')
@@ -82,14 +58,4 @@ getCorrelation = function(crimeName, crimeYear) {
 }
 
 getCorrelation('BURGLARY', '2015')
->>>>>>> refs/remotes/origin/master
 
-#year = vector(mode="numeric", length=365)
-
-
-#cor(crimeAllBURGLARYResults,weatherAllResults)
-
-#length(crimeAllBURGLARYResults)
-#length(weatherAllResults)
-
-#nrow(crimeAllBURGLARY)
